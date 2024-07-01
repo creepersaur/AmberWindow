eff756c21009fe85cce3d36e220c1bf0c6b08f77
creepersaur <69436784+creepersaur@users.noreply.github.com>
2024-07-01 16:10:41 +0530

    metadata + main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,30 @@
-use macroquad::prelude::*;
-use amberwindow::{WindowManager, WindowWidget};
+// use macroquad::prelude::*;
+// use amberwindow::{WindowManager, WindowWidget};
 
-#[macroquad::main("Window")]
-async fn main() {
-    let font: Font = load_ttf_font("src\\assets\\JetBrainsMono-Medium.ttf")
-        .await
-        .unwrap();
+// #[macroquad::main("Window")]
+// async fn main() {
+//     let font: Font = load_ttf_font("src\\assets\\JetBrainsMono-Medium.ttf")
+//         .await
+//         .unwrap();
 
-    let mut windows = WindowManager::new();
+//     let mut windows = WindowManager::new();
 
-    loop {
-        clear_background(Color::new(0.2, 0.2, 0.2, 1.0));
+//     loop {
+//         clear_background(Color::new(0.2, 0.2, 0.2, 1.0));
 
-        if let Some(win) = windows.begin("my_window", &font) {
-            win.push(
-                WindowWidget::Button("press me", &font, None, None)
-            );
+//         if let Some(win) = windows.begin("my_window", &font) {
+//             win.push(
+//                 WindowWidget::Button("press me", &font, None, None)
+//             );
     
-            if win.get_widget(0).as_button().is_just_pressed {
-                println!("BUTTON WAS JUST PRESSED!");
-            }
-        }
+//             if win.get_widget(0).as_button().is_just_pressed {
+//                 println!("BUTTON WAS JUST PRESSED!");
+//             }
+//         }
 
-        windows.update_windows();
-        next_frame().await;
-    }
-}
+//         windows.update_windows();
+//         next_frame().await;
+//     }
+// }
+
+fn main() {}
